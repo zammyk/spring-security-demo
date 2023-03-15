3b8b19baf47ba6f0fa61fc8ecc3f1606161747ae
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-03-15 12:51:14 +0530

    Add BCrypt password encoder for user password

--- src/main/java/com/rahulhembrom/springsecuritydemo/service/UserServiceImpl.java
+++ src/main/java/com/rahulhembrom/springsecuritydemo/service/UserServiceImpl.java
@@ -4,12 +4,15 @@ import com.rahulhembrom.springsecuritydemo.entity.User;
 import com.rahulhembrom.springsecuritydemo.model.UserModel;
 import com.rahulhembrom.springsecuritydemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public User registerUser(UserModel userModel) {
         User user = new User();
@@ -17,7 +20,7 @@ public class UserServiceImpl implements UserService{
         user.setRole("USER");
         user.setFirstName(userModel.getFirstName());
         user.setLastName(userModel.getLastName());
-        user.setPassword(userModel.getPassword());
+        user.setPassword(passwordEncoder.encode(userModel.getPassword()));
         userRepository.save(user);
         return user;
     }
